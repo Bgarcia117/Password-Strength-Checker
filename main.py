3493547fb54814e26839c55963f7cf59f43277c1
Bgarcia117 <bryant.garcia.1771@gmail.com>
2023-04-18 07:51:18 -0500

    Implemented a GUI using PySimpleGui

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-password = input("Enter a new password: ")
+import PySimpleGUI as sg
+
+password = sg.Text("Enter a new password: ")
+pass_box = sg.InputText(key="pass")
+button = sg.Button("Check Password")
+output = sg.Text(key="output")
+
 result = {}
 
-if len(password) >= 8:
-    result["length"] = True
-else:
-    result["length"] = False 
+window = sg.Window('Password Checker',
+                   layout=[[password, pass_box], [button, output]])
 
-digit = False
-for i in password:
-    if i.isdigit():
-        digit = True
+while True:
+    event, values = window.read()
+    # print(event, values)
+    password = values["pass"]
 
-result["digits"] = digit
+    if len(password) >= 8:
+        result["length"] = True
+    else:
+        result["length"] = False 
 
-uppercase = False
-for i in password:
-    if i.isupper():
-        uppercase = True
+    digit = False
+    for i in password:
+        if i.isdigit():
+            digit = True
 
-result["uppercase"] = uppercase
-if all(result.values()):
-    print("Strong Password")
-else:
-    print("Weak Password")
+    result["digits"] = digit
+
+    uppercase = False
+    for i in password:
+        if i.isupper():
+            uppercase = True
+
+    result["uppercase"] = uppercase
+    if all(result.values()):
+        # print("Strong Password")
+        window["output"].update(value="Strong Password", text_color="green")
+    else:
+        # print("Weak Password")
+        window["output"].update(value="Weak Password", text_color="red")
